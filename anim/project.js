a637203c5b384747b0679166a65792d25190c099
Saswat <gitsaswat@gmail.com>
2023-12-08 09:22:33 +0530

    Fixed some issue

--- anim/project.js
+++ anim/project.js
@@ -20,6 +20,6 @@ gsap.from("#projects .title", {y:50, opacity: 0, duration: 1, scrollTrigger: {
 gsap.from("#projects .project", {opacity: 0, duration: 1, scrollTrigger: {
     trigger: "#projects",
     start: "top 50%",
-    end: () => "+=" + document.querySelector(".project-con").offsetWidth,
+    end: () => "+=" + 100 + document.querySelector(".project-con").offsetWidth,
     toggleActions: "play reverse play reverse",
 }});
